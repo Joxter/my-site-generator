fe9478dc73a2cfb4e8ca28ce2e02d0296259f2e5
joxter <joxterrr@gmail.com>
2021-07-05 14:10:27 +0300

    update yolka API

--- src/yolka/yolka.js
+++ src/yolka/yolka.js
@@ -1,17 +1,21 @@
 import { parse } from "./parser.js";
 import { render } from "./render.js";
 
-export function yolka(componentStrs, pageStrs, options = {}, data = {}) {
-  // todo убрать options в самое начало?
-  if (!Array.isArray(componentStrs)) throw new Error("components should be an array");
-  if (!Array.isArray(pageStrs)) throw new Error("pages should be an array");
+export function yolka(options = {}) {
+  return function parsing(componentStrs, pageStrs) {
+    if (!Array.isArray(componentStrs)) throw new Error("components should be an array");
+    if (!Array.isArray(pageStrs)) throw new Error("pages should be an array");
 
-  const [Components, pageComponents] = parse(componentStrs, pageStrs);
+    const [Components, pageComponents] = parse(componentStrs, pageStrs);
+    // console.log(Components);
+    // console.log(pageComponents);
 
-  // console.log(Components);
-  // console.log(pageComponents);
+    return {
+      render(data = {}) {
+        const resPages = pageComponents.map(pageComponent => render(Components, pageComponent, data));
 
-  const resPages = pageComponents.map(pageComponent => render(Components, pageComponent, data));
-
-  return { pages: resPages };
+        return { pages: resPages };
+      },
+    };
+  };
 }
--- src/yolka/yolka.test.js
+++ src/yolka/yolka.test.js
@@ -1,46 +1,48 @@
 import { yolka } from "./yolka.js";
 
 describe("yolka basics", () => {
+  const defaultYolka = yolka({});
+
   it("basic parse and serializer check", () => {
-    const result = yolka([], [`<div>text<h1>header text</h1></div>`]);
+    const result = defaultYolka([], [`<div>text<h1>header text</h1></div>`]).render();
 
     expect(result.pages[0]).toEqual("<div>text<h1>header text</h1></div>");
   });
 
   it("test simple component without props", () => {
-    const result = yolka(
+    const result = defaultYolka(
       [`<template name="my-button"><button>button text</button></template>`],
       [`<h1>header</h1><div><my-button></my-button></div>`]
-    );
+    ).render();
 
     expect(result.pages[0]).toEqual("<h1>header</h1><div><button>button text</button></div>");
   });
 
   it("test simple page with data", () => {
-    const result = yolka([], [`<h1>header</h1><p>hello, {name}!</p>`], {}, { name: "Kolya" });
+    const result = defaultYolka([], [`<h1>header</h1><p>hello, {name}!</p>`]).render({ name: "Kolya" });
 
     expect(result.pages[0]).toEqual("<h1>header</h1><p>hello, Kolya!</p>");
   });
 
   it("test simple page with deep data", () => {
-    const result = yolka([], [`<h1>header</h1><p>hello, {users[0].name}!</p>`], {}, { users: [{ name: "Kolya" }] });
+    const result = defaultYolka([], [`<h1>header</h1><p>hello, {users[0].name}!</p>`]).render({
+      users: [{ name: "Kolya" }],
+    });
 
     expect(result.pages[0]).toEqual("<h1>header</h1><p>hello, Kolya!</p>");
   });
 
   it("test simple component with props", () => {
-    const result = yolka(
+    const result = defaultYolka(
       [`<template name="my-greeting" props="user"><p>hello, {user.name}!</p></template>`],
-      [`<h1>header</h1><div><my-greeting user="{users[0]}"></my-greeting></div>`],
-      {},
-      { users: [{ name: "Kolya" }] }
-    );
+      [`<h1>header</h1><div><my-greeting user="{users[0]}"></my-greeting></div>`]
+    ).render({ users: [{ name: "Kolya" }] });
 
     expect(result.pages[0]).toEqual("<h1>header</h1><div><p>hello, Kolya!</p></div>");
   });
 
   it("test several instances of component", () => {
-    const result = yolka(
+    const result = defaultYolka(
       [`<template name="my-greeting" props="user"><p>hello, {user.name}!</p></template>`],
       [
         `<h1>header</h1>
@@ -48,10 +50,8 @@ describe("yolka basics", () => {
   <my-greeting user="{users[0]}"></my-greeting>
   <my-greeting user="{users[1]}"></my-greeting>
 </div>`,
-      ],
-      {},
-      { users: [{ name: "Kolya" }, { name: "Petya" }] }
-    );
+      ]
+    ).render({ users: [{ name: "Kolya" }, { name: "Petya" }] });
 
     expect(result.pages[0]).toEqual(`<h1>header</h1>
 <div>
@@ -61,7 +61,7 @@ describe("yolka basics", () => {
   });
 
   it("test several components", () => {
-    const result = yolka(
+    const result = defaultYolka(
       [
         `<template name="my-header" props="user"><p>my best site for {user.name}!</p></template>`,
         `<template name="my-greeting" props="user"><p>hello, {user.name}!</p></template>`,
@@ -72,15 +72,23 @@ describe("yolka basics", () => {
   <my-header user="{users[0]}"></my-header>
   <my-greeting user="{users[0]}"></my-greeting>
 </div>`,
-      ],
-      {},
-      { users: [{ name: "Kolya" }] }
-    );
+      ]
+    ).render({ users: [{ name: "Kolya" }] });
 
     expect(result.pages[0]).toEqual(`<h1>header</h1>
 <div>
   <p>my best site for Kolya!</p>
   <p>hello, Kolya!</p>
 </div>`);
+  });
+
+  it("test several renders with different data", () => {
+    const pages = defaultYolka(
+      [`<template name="my-greeting" props="user"><p>hello, {user.name}!</p></template>`],
+      [`<div><my-greeting user="{users[0]}"></my-greeting></div>`]
+    );
+
+    expect(pages.render({ users: [{ name: "Kolya" }] }).pages[0]).toEqual(`<div><p>hello, Kolya!</p></div>`);
+    expect(pages.render({ users: [{ name: "Petya" }] }).pages[0]).toEqual(`<div><p>hello, Petya!</p></div>`);
   });
 });
